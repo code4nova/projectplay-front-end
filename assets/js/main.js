7dbb77cb61b06a8ac8fb7826ecc3f57420abd4ca
Edzon Quinteros <edzon.quinteros@gmail.com>
2018-07-26 16:06:51 -0400

    Completed project play static site

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  $(window).scroll(function() {
+    if($(this).scrollTop() >=10) {
+      $('nav').addClass('sticky');
+      $('.side-image  img').css({
+        'max-height':'100px', 
+        'margin-top':'5px',
+      });
+    } else {
+      $('nav').removeClass('sticky');
+      $('.side-image  img').css({
+        'max-height':'220px', 
+        'margin-top':'0px',
+      });
+    }
+    if($(window).width() <=900){
+      $('.side-image  img').css({
+        'max-height':'100px',
+      });
+    } 
+  });
+  $('#btn-menu').click(function() {
+     $('.menu').toggleClass("open-menu");
+  });    
+});
+function myMap() {
+  var mapOptions = {
+      center: new google.maps.LatLng(39, -77),
+      zoom: 12,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
